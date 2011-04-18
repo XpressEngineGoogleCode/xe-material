42de98d648fceda334a4e4b5f7c6ad946577bd43
ovclas <ovclas@ba077f73-4b38-3732-a142-2c0ecc349a9c>
2011-04-18 05:57:27 +0000

    init 1.1

---
+++ branches/1.1/lang/tr.lang.php
@@ -0,0 +1,53 @@
+<?php
+    /**
+     * @file   en.lang.php
+     * @author nicetwo
+     * @brief  default language pack of ingredients collector (english)
+     **/
+
+    $lang->material->popup_title   = 'Textyle yazılı İçerik Toplayıcısı';
+    $lang->material->text   = 'Metin';
+    $lang->material->images = 'Resim';
+    $lang->material->link   = 'Köprü';
+    $lang->material->list   = 'Liste';
+    $lang->material->cite   = 'Alıntı';
+    $lang->material->movie  = 'Video Klibi';
+
+	$lang->material->cmd_deposit = 'Topla';
+
+    $lang->material->subject  = 'Konu';
+    $lang->material->about_subject  = 'Lütfen paragraf başlığını giriniz.';
+    $lang->material->about_page_subject  = 'Lütfen köprü başlığını giriniz.';
+    $lang->material->content  = 'İçerik';
+    $lang->material->about_content  = 'Lütfen içerik giriniz.';
+
+    $lang->material->about_linksubject  = 'Köprünün yönlendirdiği sayfanın başlığını giriniz.';
+    $lang->material->about_linkdescription  = 'Ek tanımlama girebilirsiniz.';
+    $lang->material->description  = 'Tanım';
+    $lang->material->about_list  = 'Lütfen liste giriniz.';
+
+    $lang->material->about_citesubject  = 'Lütfen alıntı giriniz.';
+    $lang->material->citesubject  = 'Alıntı';
+    $lang->material->citefrom  = 'Kaynak';
+    $lang->material->about_citefrom  = 'Kaynak girebilirsiniz.';
+
+    $lang->msg_fail_image_save  = 'Resmi depolarken bir sorun oluştu.';
+    $lang->msg_not_select_image  = 'Hedef resim seçilmedi.';
+    $lang->material->select_image  = 'Bir resim seçiniz';
+    $lang->material->about_imagedescription  = 'Resim için tanım girebilirsiniz.';
+
+    $lang->material->code  = 'Kod';
+    $lang->material->about_code  = '<object> veya <embed> ile başlayan çoklu ortam ekleme kodu giriniz';
+    $lang->material->codedescription  = 'Altbaşlık veya Tanım';
+    $lang->material->about_codedescription  = 'Video klip için tanım girebilirsiniz.';
+
+
+	$lang->material->error_msg_title = "Özür dileriz. Tarayıcı Düğmesi (bookmarklet) özelliği bir hatadan dolayı çalışmamaktadır. Lütfen aşağıdaki muhtemel sebepleri kontrol ediniz.";
+	$lang->material->error_msg_list = array(
+		"Sunucu veya sunucu servisi çalışmamaktadır.",
+		"Üyeliğiniz, siteden silinmiştir",
+		"Tarayıcı Düğmesi köprü hatası"
+		);
+
+
+?>
